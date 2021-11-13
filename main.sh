4cd07c987d911fb6021dcedf67cfcd0893bf0132
umeshtyagi829 <umeshtyagi829@gmail.com>
2021-11-13 11:31:07 +0530

    update main.sh

--- main.sh
+++ main.sh
@@ -1,25 +1,21 @@
-docker run -dp 3306:3306 --name mydb \
--e MYSQL_USER=umesh \
+#!/bin/bash
+
+sh 'sudo docker stop mydb'
+sh 'sudo docker rm mydb'
+                
+sh 'sudo docker stop mywebapp'
+sh 'sudo docker rm mywebapp'
+
+sh '''sudo docker run -dp 3306:3306 --name mydb \
+-e MYSQL_USER=$MYSQL_CREDENTIALS_USR \
 -e MYSQL_DATABASE=todo \
--e MYSQL_ROOT_PASSWORD=Umesh@123 \
--e MYSQL_PASSWORD=Umesh@123 \
-custom-mysql
+-e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+-e MYSQL_PASSWORD=$MYSQL_CREDENTIALS_PSW \
+mysql_by_jenkins'''
 
-
-docker run -d -p 8080:80 --link mydb:mydb --name mywebapp \
--e MYSQL_USER=umesh \
+sh '''sudo docker run -d -p 8000:80 --link mydb:mydb --name mywebapp \
+-e MYSQL_USER=$MYSQL_CREDENTIALS_USR \
 -e MYSQL_DATABASE=todo \
 -e DB_HOST=mydb \
--e MYSQL_PASSWORD=Umesh@123 \
-custom-php
-
-#change the dir to /code then only run this command
-docker run -d -p 8080:80 --link mydb:mydb --name mywebapp -v $(PWD):/var/www/html/ \
--e MYSQL_USER=umesh \
--e MYSQL_DATABASE=todo \
--e DB_HOST=mydb \
--e MYSQL_PASSWORD=Umesh@123 \
-custom-php
-
-
-
+-e MYSQL_PASSWORD=$MYSQL_CREDENTIALS_PSW \
+php_by_jenkins'''
