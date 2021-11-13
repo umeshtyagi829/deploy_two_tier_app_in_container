4a0479d24a76181d174928c3e488e33e9022ab31
umeshtyagi829 <umeshtyagi829@gmail.com>
2021-11-13 14:01:45 +0530

    update main.sh

--- main.sh
+++ main.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-sh 'sudo docker stop mydb'
-sh 'sudo docker rm mydb'
+sudo docker stop mydb
+sudo docker rm mydb
                 
-sh 'sudo docker stop mywebapp'
-sh 'sudo docker rm mywebapp'
+sudo docker stop mywebapp
+sudo docker rm mywebapp
 
-sh '''sudo docker run -dp 3306:3306 --name mydb \
+sudo docker run -dp 3306:3306 --name mydb \
 -e MYSQL_USER=$MYSQL_CREDENTIALS_USR \
 -e MYSQL_DATABASE=todo \
 -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
 -e MYSQL_PASSWORD=$MYSQL_CREDENTIALS_PSW \
-mysql_by_jenkins'''
+mysql_by_jenkins
 
-sh '''sudo docker run -d -p 8000:80 --link mydb:mydb --name mywebapp \
+sudo docker run -d -p 8000:80 --link mydb:mydb --name mywebapp \
 -e MYSQL_USER=$MYSQL_CREDENTIALS_USR \
 -e MYSQL_DATABASE=todo \
 -e DB_HOST=mydb \
 -e MYSQL_PASSWORD=$MYSQL_CREDENTIALS_PSW \
-php_by_jenkins'''
+php_by_jenkins
